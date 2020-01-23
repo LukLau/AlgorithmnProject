8e5098297eda5feaa3bdf6463474462959edfc9d
liulu <liulu12@xiaomi.com>
2020-01-24 00:06:09 +0800

    feat: 01/23
    295. Find Median from Data Stream
    key point: 使用两个队列来维持 保证内存空间

--- src/main/java/org/dora/algorithm/geeksforgeek/MedianFinder.java
+++ src/main/java/org/dora/algorithm/geeksforgeek/MedianFinder.java
@@ -1,25 +1,38 @@
 package org.dora.algorithm.geeksforgeek;
 
+import java.util.PriorityQueue;
+
 /**
  * @author liulu12@xiaomi.com
  * @date 2020/1/21
  */
 public class MedianFinder {
 
+    private PriorityQueue<Integer> max = new PriorityQueue<>();
+    private PriorityQueue<Integer> min = new PriorityQueue<>();
 
     /**
      * initialize your data structure here.
      */
     public MedianFinder() {
+    }
+
+    public static void main(String[] args) {
 
     }
 
     public void addNum(int num) {
-
+        max.offer(num);
+        min.offer(-max.poll());
+        if (max.size() < min.size()) {
+            max.offer(-min.poll());
+        }
     }
 
     public double findMedian() {
-        return -1;
+        return max.size() > min.size()
+                ? max.peek()
+                : (max.peek() - min.peek()) / 2.0;
     }
 
 }
