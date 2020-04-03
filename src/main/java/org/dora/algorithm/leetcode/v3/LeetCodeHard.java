5e21f20cf8da6e3f8cb48609a1889a8368caf852
liulu <liulu12@xiaomi.com>
2020-04-03 21:59:05 +0800

    feat 04/03

--- src/main/java/org/dora/algorithm/leetcode/v3/LeetCodeHard.java
+++ src/main/java/org/dora/algorithm/leetcode/v3/LeetCodeHard.java
@@ -124,4 +124,61 @@ public class LeetCodeHard {
     }
 
 
+    /**
+     * 10. Regular Expression Matching
+     *
+     * @param s
+     * @param p
+     * @return
+     */
+    public boolean isMatch(String s, String p) {
+        if (s == null) {
+            return true;
+        }
+        if (p == null) {
+            return false;
+        }
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    if (p.charAt(j - 2) != '.' && p.charAt(j - 2) != s.charAt(i - 1)) {
+                        dp[i][j] = dp[i][j - 2];
+                    } else {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 1] || dp[i][j - 2];
+                    }
+
+                }
+
+            }
+        }
+        return dp[m][n];
+    }
+
+
+    public boolean isMatchV2(String s, String p) {
+        if (s.isEmpty()) {
+            return p.isEmpty();
+        }
+        boolean firstMatch = !p.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+
+        if (s.length() >= 2 && p.charAt(1) == '*') {
+            return isMatchV2(s, p.substring(2)) ||
+                    (firstMatch && isMatchV2(s.substring(1), p));
+        } else {
+            return firstMatch && isMatchV2(s.substring(1), p.substring(1));
+        }
+
+
+    }
+
+
 }
